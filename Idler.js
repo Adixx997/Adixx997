70d7cdf8801e0dec8bb19abb848bdf7ab22a7f0c
Adixx997 <89809285+Adixx997@users.noreply.github.com>
2021-08-30 22:16:24 +0200

    Add files via upload

---
+++ Idler.js
@@ -0,0 +1,25 @@
+    var steamClientFactory = require('./steamClient.js');
+    var configsArray = [];
+    var config;
+    var botArray = [];
+    
+    config = {};
+    config.username = 's0xsfj3c847jsmien';
+    config.password = '42ZJQI1O9TSR090';
+    config.sharedSecret = ''; //Shared Secret(for 2FA only) https://github.com/Jessecar96/SteamDesktopAuthenticator/releases
+    config.games = [730]
+    configsArray.push(config);
+	
+	
+	
+    console.log('Number of configurations set up: ' + configsArray.length);
+     
+    for	(index = 0; index < configsArray.length; index++) {
+    	var config = configsArray[index];
+		
+		var bot = steamClientFactory.buildBot(config);
+		bot.doLogin();
+		botArray.push(bot);
+    }
+     
+    console.log('Running ' + botArray.length + ' bots.');
